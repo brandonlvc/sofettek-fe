31828660583f0942147ed0c0e9c2f3af428f6675
unknown <bvitorc@uni.pe>
2024-10-07 06:56:18 -0500

    test deploy

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,4 +14,3 @@ root.render(
 
 
 
-reportWebVitals();
